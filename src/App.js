dafa1d67b2f97464252528c7fc8159764048dda0
michael-krontz <michaelkrontz@gmail.com>
2021-11-30 16:56:01 -0500

    Front End Skeleton

--- src/App.js
+++ src/App.js
@@ -1,13 +1,78 @@
-import logo from './logo.svg';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faNewspaper } from '@fortawesome/free-solid-svg-icons'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { faRetweet } from '@fortawesome/free-solid-svg-icons'
+
+
+const headlineIcon = <FontAwesomeIcon icon={faNewspaper} />
+const searchIcon = <FontAwesomeIcon icon={faSearch} />
+const tweetIcon = <FontAwesomeIcon icon={faRetweet} />
+
+function CoinCard() {
+  return (
+    <div className="coin-card">
+        <div className="coin-card-header">
+        <div className="coin-rank"></div>
+          <h2 className="coin-ticker">DOGE</h2>
+          <button className="buy-button">Buy</button>
+        </div>
+
+        <div className="coin-card-content">
+          <div className="coin-card-content-left">
+            <div className="coin-name">Dogecoin</div>
+            <div className="coin-price-usd">$0.22</div>
+            <div className="coin-price-ticker">+$0.000521 (0.24%)</div>
+          </div>
+          <div className="coin-card-content-right">
+            <div className="stat-wrapper">
+              <div className="headline-stat">7</div>
+              <div className="headline-icon">{headlineIcon}</div>
+            </div>
+
+            <div className="stat-wrapper">
+              <div className="twitter-stat">9.24k</div>
+              <div className="twitter-icon">{tweetIcon}</div>
+            </div>
+
+            <div className="stat-wrapper">
+              <div className="google-trend-stat">29k</div>
+              <div className="google-icon">{searchIcon}</div>
+            </div>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+function JoinCard() {
+  return (
+    <div className="join-card">
+      Coin Brawl Intro &
+      Member Benefits
+    </div>
+  )
+}
+
+
+function JoinButton() {
+  return (
+    <button className="join-button">Join</button>
+  )
+}
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h2>Coin Brawl</h2>
-        <img src={logo} className="App-logo" alt="logo" />
+        Coin Brawl
       </header>
+      <CoinCard></CoinCard>
+      <CoinCard></CoinCard>
+      <CoinCard></CoinCard>
+      <JoinCard></JoinCard>
+      <JoinButton></JoinButton>
     </div>
   );
 }
