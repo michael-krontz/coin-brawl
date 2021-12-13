8e1ec005c424afbe8bce29fbea1078fa5057d19f
michael-krontz <michaelkrontz@gmail.com>
2021-12-13 17:36:38 -0500

    WIP

--- src/bot.js
+++ src/bot.js
@@ -6,10 +6,17 @@ const main = async () => {
   const getTimestampRl = await API.rest.Others.getTimestamp();
   console.log(getTimestampRl.data);
 
-  // const res = await API.rest.Trade.Orders.postOrder({ clientOid: 'qwerty07070707', side: 'buy', symbol: 'DOGE-USDT', type: 'market', size: 18.75 });
+// Buy
+//   const res = await API.rest.Trade.Orders.postOrder({ clientOid: 'qwerty07070707', side: 'buy', symbol: 'DOGE-USDT', type: 'market', funds: 24 });
+
+// Sell
+//   const res = await API.rest.Trade.Orders.postOrder({ clientOid: 'qwerty07070707', side: 'sell', symbol: 'DOGE-USDT', type: 'market', funds: 24 });
+
+// Get Fiat Price List
   const res = await API.rest.Market.Currencies.getFiatPrice();
   console.log(res);
 };
 
 // run rest main
 main();
+
